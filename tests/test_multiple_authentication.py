a68eace9b132381ecda2672f387a2c49a3deb52f
Colin-b <Colin-b@users.noreply.github.com>
2019-11-12 17:44:11 +0100

    Add more authorization code tests

--- tests/test_multiple_authentication.py
+++ tests/test_multiple_authentication.py
@@ -1,7 +1,12 @@
 from responses import RequestsMock
 
 import requests_auth
-from tests.oauth2_helper import authenticated_service, token_cache, TIMEOUT
+from tests.oauth2_helper import (
+    authenticated_service,
+    token_cache,
+    TIMEOUT,
+    TEST_SERVICE_HOST,
+)
 from tests.auth_helper import get_header
 
 
@@ -133,6 +138,62 @@ def test_oauth2_client_credential_and_multiple_authentication_can_be_combined(
     )
     header = get_header(
         responses, resource_owner_password_auth + (api_key_auth + api_key_auth2)
+    )
+    assert header.get("Authorization") == "Bearer 2YotnFZFEjr1zCsicMWpAA"
+    assert header.get("X-Api-Key") == "my_provided_api_key"
+    assert header.get("X-Api-Key2") == "my_provided_api_key2"
+
+
+def test_oauth2_authorization_code_and_api_key_authentication_can_be_combined(
+    authenticated_service, token_cache, responses: RequestsMock
+):
+    authorization_code_auth = requests_auth.OAuth2AuthorizationCode(
+        TEST_SERVICE_HOST + "/provide_code_as_anchor_code",
+        "http://provide_access_token",
+        timeout=TIMEOUT,
+    )
+    responses.add(
+        responses.POST,
+        "http://provide_access_token",
+        json={
+            "access_token": "2YotnFZFEjr1zCsicMWpAA",
+            "token_type": "example",
+            "expires_in": 3600,
+            "refresh_token": "tGzv3JOkF0XG5Qx2TlKWIA",
+            "example_parameter": "example_value",
+        },
+    )
+    api_key_auth = requests_auth.HeaderApiKey("my_provided_api_key")
+    header = get_header(responses, authorization_code_auth + api_key_auth)
+    assert header.get("Authorization") == "Bearer 2YotnFZFEjr1zCsicMWpAA"
+    assert header.get("X-Api-Key") == "my_provided_api_key"
+
+
+def test_oauth2_authorization_code_and_multiple_authentication_can_be_combined(
+    authenticated_service, token_cache, responses: RequestsMock
+):
+    authorization_code_auth = requests_auth.OAuth2AuthorizationCode(
+        TEST_SERVICE_HOST + "/provide_code_as_anchor_code",
+        "http://provide_access_token",
+        timeout=TIMEOUT,
+    )
+    responses.add(
+        responses.POST,
+        "http://provide_access_token",
+        json={
+            "access_token": "2YotnFZFEjr1zCsicMWpAA",
+            "token_type": "example",
+            "expires_in": 3600,
+            "refresh_token": "tGzv3JOkF0XG5Qx2TlKWIA",
+            "example_parameter": "example_value",
+        },
+    )
+    api_key_auth = requests_auth.HeaderApiKey("my_provided_api_key")
+    api_key_auth2 = requests_auth.HeaderApiKey(
+        "my_provided_api_key2", header_name="X-Api-Key2"
+    )
+    header = get_header(
+        responses, authorization_code_auth + (api_key_auth + api_key_auth2)
     )
     assert header.get("Authorization") == "Bearer 2YotnFZFEjr1zCsicMWpAA"
     assert header.get("X-Api-Key") == "my_provided_api_key"
--- tests/test_oauth2_authorization_code.py
+++ tests/test_oauth2_authorization_code.py
@@ -1,4 +1,5 @@
 from responses import RequestsMock
+import pytest
 
 import requests_auth
 from tests.oauth2_helper import (
@@ -7,7 +8,7 @@ from tests.oauth2_helper import (
     TIMEOUT,
     TEST_SERVICE_HOST,
 )
-from tests.auth_helper import get_header
+from tests.auth_helper import get_header, get_request
 
 
 def test_oauth2_authorization_code_flow_get_code_is_sent_in_authorization_header_by_default(
@@ -33,3 +34,82 @@ def test_oauth2_authorization_code_flow_get_code_is_sent_in_authorization_header
         get_header(responses, auth).get("Authorization")
         == "Bearer 2YotnFZFEjr1zCsicMWpAA"
     )
+    assert (
+        get_request(responses, "http://provide_access_token/").body
+        == "grant_type=authorization_code&redirect_uri=http%3A%2F%2Flocalhost%3A5000%2F&response_type=code&code=SplxlOBeZQQYbYS6WxSbIA"
+    )
+
+
+def test_nonce_is_sent_if_provided_in_authorization_url(
+    authenticated_service, token_cache, responses: RequestsMock
+):
+    auth = requests_auth.OAuth2AuthorizationCode(
+        TEST_SERVICE_HOST + "/provide_code_as_anchor_code?nonce=123456",
+        "http://provide_access_token",
+        timeout=TIMEOUT,
+    )
+    responses.add(
+        responses.POST,
+        "http://provide_access_token",
+        json={
+            "access_token": "2YotnFZFEjr1zCsicMWpAA",
+            "token_type": "example",
+            "expires_in": 3600,
+            "refresh_token": "tGzv3JOkF0XG5Qx2TlKWIA",
+            "example_parameter": "example_value",
+        },
+    )
+    assert (
+        get_header(responses, auth).get("Authorization")
+        == "Bearer 2YotnFZFEjr1zCsicMWpAA"
+    )
+    assert (
+        get_request(responses, "http://provide_access_token/").body
+        == "grant_type=authorization_code&redirect_uri=http%3A%2F%2Flocalhost%3A5000%2F&response_type=code&code=SplxlOBeZQQYbYS6WxSbIA"
+    )
+
+
+def test_response_type_can_be_provided_in_url(
+    authenticated_service, token_cache, responses: RequestsMock
+):
+    auth = requests_auth.OAuth2AuthorizationCode(
+        TEST_SERVICE_HOST + "/provide_code_as_anchor_code?response_type=code",
+        "http://provide_access_token",
+        timeout=TIMEOUT,
+        response_type="not_used",
+    )
+    responses.add(
+        responses.POST,
+        "http://provide_access_token",
+        json={
+            "access_token": "2YotnFZFEjr1zCsicMWpAA",
+            "token_type": "example",
+            "expires_in": 3600,
+            "refresh_token": "tGzv3JOkF0XG5Qx2TlKWIA",
+            "example_parameter": "example_value",
+        },
+    )
+    assert (
+        get_header(responses, auth).get("Authorization")
+        == "Bearer 2YotnFZFEjr1zCsicMWpAA"
+    )
+
+
+def test_authorization_url_is_mandatory():
+    with pytest.raises(Exception) as exception_info:
+        requests_auth.OAuth2AuthorizationCode("", "http://test_url")
+    assert str(exception_info.value) == "Authorization URL is mandatory."
+
+
+def test_token_url_is_mandatory():
+    with pytest.raises(Exception) as exception_info:
+        requests_auth.OAuth2AuthorizationCode("http://test_url", "")
+    assert str(exception_info.value) == "Token URL is mandatory."
+
+
+def test_header_value_must_contains_token():
+    with pytest.raises(Exception) as exception_info:
+        requests_auth.OAuth2AuthorizationCode(
+            "http://test_url", "http://test_url", header_value="Bearer token"
+        )
+    assert str(exception_info.value) == "header_value parameter must contains {token}."
