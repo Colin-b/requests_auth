3a52d66128597ecccf63ec9d991529adca40dba1
Colin-b <Colin-b@users.noreply.github.com>
2019-12-12 18:30:03 +0100

    Reproduce failure due to expires_in sent as str

--- tests/test_oauth2_client_credential.py
+++ tests/test_oauth2_client_credential.py
@@ -30,6 +30,27 @@ def test_oauth2_client_credentials_flow_token_is_sent_in_authorization_header_by
     )
 
 
+def test_expires_in_sent_as_str(token_cache, responses: RequestsMock):
+    auth = requests_auth.OAuth2ClientCredentials(
+        "http://provide_access_token", client_id="test_user", client_secret="test_pwd"
+    )
+    responses.add(
+        responses.POST,
+        "http://provide_access_token",
+        json={
+            "access_token": "2YotnFZFEjr1zCsicMWpAA",
+            "token_type": "example",
+            "expires_in": "3600",
+            "refresh_token": "tGzv3JOkF0XG5Qx2TlKWIA",
+            "example_parameter": "example_value",
+        },
+    )
+    assert (
+        get_header(responses, auth).get("Authorization")
+        == "Bearer 2YotnFZFEjr1zCsicMWpAA"
+    )
+
+
 def test_with_invalid_grant_request_no_json(token_cache, responses: RequestsMock):
     auth = requests_auth.OAuth2ClientCredentials(
         "http://provide_access_token", client_id="test_user", client_secret="test_pwd"
