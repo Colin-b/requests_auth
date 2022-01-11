84aed0973c08b4ec0a0427fb452eddaa20626fd5
Colin-b <Colin-b@users.noreply.github.com>
2022-01-11 18:58:28 +0100

    Add early_expiry to OktaAuthorizationCode

--- requests_auth/authentication.py
+++ requests_auth/authentication.py
@@ -1018,6 +1018,9 @@ class OktaAuthorizationCode(OAuth2AuthorizationCode):
         token by default.
         :param token_field_name: Name of the expected field containing the token.
         access_token by default.
+        :param early_expiry: Number of seconds before actual token expiry where token will be considered as expired.
+        Default to 30 seconds to ensure token will not expire between the time of retrieval and the time the request
+        reaches the actual server. Set it to 0 to deactivate this feature and use the same token until actual expiry.
         :param nonce: Refer to http://openid.net/specs/openid-connect-core-1_0.html#IDToken for more details
         (formatted as an Universal Unique Identifier - UUID). Use a newly generated UUID by default.
         :param authorization_server: Okta authorization server
